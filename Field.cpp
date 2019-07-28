a1d6ceea6441acb54128886e3a571a89c10eb279
AlisiaSmit <anya0192837465@ya.ru>
2019-07-29 00:11:09 +0700

    the field is painted

--- Field.cpp
+++ Field.cpp
@@ -2,9 +2,8 @@
 
 using namespace std;
 
-Field::Field(int SIZE)
+Field::Field(int SIZE) : size(SIZE)
 {
-	size = SIZE;
 	Snake* severus = new Snake();
 	objects.push_back(severus);
 	map = new char*[size];
@@ -15,6 +14,7 @@ Field::Field(int SIZE)
 		{
 			map[i][j] = '#';
 		}
+		map[i][size] = '\0';
 	}
 }
 
@@ -22,29 +22,28 @@ Field::Field(int SIZE)
 Field::~Field()
 {
 	for (IObject* ob : objects)
-	{
 		delete ob;
-	}
+	
+	
+	for (int i = 0; i < size; i++)
+		delete map[i];
+	delete map;
+
 }
 
 char** Field::UpdateMap()
 {
 	for (int i = 0; i < size; i++)
-	{
 		for (int j = 0; j < size; j++)
-		{
 			map[i][j] = '#';
-		}
-	}
+	
 	for (IObject* ob : objects)
 	{
 		std::vector<XY> shape = ob->GetShape();
 		char sym = ob->GetSymbol();
 
 		for (XY coords : shape)
-		{
 			map[coords.x][coords.y] = sym;
-		}
 	}
 	return map;
 }
--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
 #include<iostream>
+#include<sstream>
 #include<string>
 #include "Field.h"
 
-#define MAP_SIZE 6
+#define MAP_SIZE 20
 
 using namespace std;
 
 int main()
 {
+	ostringstream oss;
+
 	Field field(MAP_SIZE);
 	char** map = field.UpdateMap();
 	for (int i = 0;i< MAP_SIZE;i++)
 	{
-		string currStr = "";
-		currStr.append(map[i]);
-		cout << currStr << endl;
+		//string currStr = "";
+		//currStr.append(map[i]);
+		//cout << currStr << endl;
+		oss << map[i] << endl;
 	}
+	cout << oss.str();
 	cin.get();
 }
